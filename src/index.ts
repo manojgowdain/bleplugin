75113a5368853c4e44de2e5c71a98d2602865655
ManojGowda89 <mail@manojgowda.in>
2026-07-21 11:30:33 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { default } from './BlekitModule';
-export { BleManager } from './BleManager';
+export { BleManager as blekit2 } from './BleManager';
+export { BleManager as blekit } from "react-native-ble-plx";
 export { Device } from './Device';
 export { Service } from './Service';
 export { Characteristic } from './Characteristic';
